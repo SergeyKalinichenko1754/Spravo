0adfaf24019658fbf922f43923f1da8a517b1f57
Sergey Kalinichenko <skalinichenko.mail@gmail.com>
2019-09-24 20:24:19 +0300

    Initial commit

---
+++ Spravo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spravo
+//
+//  Created by Onix on 9/24/19.
+//  Copyright © 2019 Home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
